2170998f16cede43633b64ae180fd8680e388dd1
Ville Tuulos <ville@outerbounds.co>
2025-01-20 06:21:41 +0000

    add snowpark example

---
+++ snowparkflow_example.py
@@ -0,0 +1,40 @@
+from metaflow import FlowSpec, step, card, Parameter, trigger_on_finish, project, current, pypi, Config, config_expr
+from metaflow import snowpark
+from metaflow.cards import Table, Markdown
+
+from flowproject import BaseFlow
+
+@project(name=config_expr("flowconfig.project_name"))
+@trigger_on_finish(flow='SensorFlow')
+class SnowparkFlow(BaseFlow):
+
+    snowpark_config = Config("snowpark_config", default="snowpark.json")
+    snowpark_session = Config("snowpark_session", default="snowpark_session.json")
+    tablename = Parameter('tablename', default='villetesttable')
+
+    @pypi(python='3.11', packages={'snowflake': '1.0.2',
+                                   'pandas': '2.2.3',
+                                   'snowflake-snowpark-python': '1.26.0'})
+    @snowpark(**snowpark_config)
+    @card
+    @step
+    def start(self):
+        # this step executes inside snowflake
+        from snowflake.snowpark import Session
+        sess = Session.builder.configs(self.snowpark_session).create()
+        df = sess.table(self.tablename)
+        self.rows = []
+        self.cols = df.columns
+        for row in df.to_local_iterator():
+            d = row.as_dict()
+            self.rows.append([d[c] for c in self.cols])
+        current.card.append(Markdown(f'# Table `{self.tablename}`'))
+        current.card.append(Table(self.rows, headers=self.cols))
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+if __name__ == "__main__":
+    SnowparkFlow()
